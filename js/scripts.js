8b4a081ca79510769d01682d04eb7f1f31ae1c48
Snulu <Nnabwami@gmail.com>
2019-01-22 19:38:04 -0800

    display user inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
   $("#things form").submit(function(event) {
-    var favoritesId = ["#favorite1", "#favorite2", "#favorite3"];
+    var favoritesId = ["favorite2", "favorite1", "favorite3"];
     var userInputes = [];
     console.log("things ", favoritesId);
 
     favoritesId.forEach(function(favoriteId) {
       //debugger;
-      var userInput = $(favoriteId).val();
+      var userInput = $("#" + favoriteId).val();
+      $("." + favoriteId).text(userInput).val();
       userInputes.push(userInput);
+
       // console.log(userInput);
       // $("." + thingsId).text(userInput).val();
     });
 
     // $("#story").show();
     console.log("User Inputes: ", userInputes);
-    console.log("first element: ", userInputes[1]);
-    console.log("first element: ", userInputes[0]);
-    console.log("first element: ", userInputes[2]);
+    // console.log("first element: ", userInputes[1]);
+    // console.log("first element: ", userInputes[0]);
+    // console.log("first element: ", userInputes[2]);
     event.preventDefault();
   });
 });
